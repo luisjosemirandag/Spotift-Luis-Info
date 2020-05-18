f5085c32977ab4630ebd17db3b41d693044f86ca
luisjosemirandag <55555049+luisjosemirandag@users.noreply.github.com>
2020-05-18 13:24:28 -0300

    Add files via upload

--- Spotify project/Spotify info/Albums/albums_info.py
+++ Spotify project/Spotify info/Albums/albums_info.py
@@ -4,9 +4,9 @@ import requests
 import pandas as pd
 import codecs
 # Read de songs ids csv
-albums = pd.read_csv('Spotify project/Scrobber history plays/search_info_spotify.csv')
+albums = pd.read_csv('Projects/Spotify project/New data/New_albums.csv')
 # Separate the csv in the columns in need
-albums_id = albums.iloc[:,2]
+albums_id = albums.iloc[:,0]
 
 # Insert the column artust into a list so it's easier to process
 albums_id_list = [albums_id[x] for x in range(0,len(albums_id))]
@@ -17,7 +17,7 @@ albums_id_list = list(dict.fromkeys(albums_id_list))
 album_length = len(albums_id_list)
 
 # Declare the current index so it's easier to re-execute
-a = open('Spotify project/Spotify info/Albums/current_index.txt','r')
+a = open('Projects/Spotify project/Spotify info/Albums/current_index.txt','r')
 current_index = int(a.read())
 
 try:
@@ -28,7 +28,7 @@ try:
         headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer BQDIdIjTTdz8gO-8Lzm9JP74dS_oD2CkdD6iLBr11ogHdb5syB3fQeg4UJLoJJFPf958-hcoa6sEysLZERW-RlcWr5KFYTxJWmwhXJSgmr9Cn3DlIWF_co4LhW-X0HrqysttY1cjX43jHjhS1F0oGPg3Zu7No_8Yk7Wnfg17kGKHC5gWsjcEUxgDP3Ax33IjMpbXbNRATmNQTj4148TwiwmcfFcVsEm2jkdaF5yUR-qnZOV0d7txPlSwtz6HBWoQoNaf8Dho0CJ6YB4g984BpQjp_4X6GZL4hQ',
+            'Authorization': 'Bearer BQCmumapntkebFwdo6TIukw-cJ91HGOXrJxa-ZW9yr_wLxM2V_GyNHEx6FMUso4yVFdM_Fbo6xvo-FV1tIQWyPTGCRsNtc2F59dFVvy8X53tBzlyec_Q4fOuD1ij-vl5KaeLrLgytYhB-lzyBvfxBcW8smNpPQzla0hIkDvNt-96AH2Gdc3r9YHZIlN83FaCn9gyposcwsXyqBoCVf1aW_6Y7xJiyBQyxmew8jho4waM3FYUrflE6ZkiH14HjvqNbFsENOrU-tKRZguz2YQl6E9mDu7EM8UwLPAm',
         }
         
         # The page with the id defined:
@@ -52,26 +52,26 @@ try:
         
         def album_picture(page):
             images_1 = pagina_data.get("images")
-            images_3 = images_2[0]
+            images_3 = images_1[0]
             images_final = images_3.get("url")
             return images_final
 
         # write all in a csv file
-        b = open('Spotify project/Spotify info/Albums/scrobber_info_spotify_albums.csv', 'a',encoding='utf-8')
+        b = open('Projects/Spotify project/Spotify info/Albums/scrobber_info_spotify_albums.csv', 'a',encoding='utf-8')
         b.write(
-        str(albums_id[x]) + "," +
+        str(albums_id_list[x]) + "," +
         str(album_name) + ", " +
         str(album_popularity) + "," +
         str(album_release_date) + "," +
         str(album_picture(pagina_data)) + '\n')
         b.close()
 except:
-    f = open('Spotify project/Spotify info/Albums/current_index.txt','w')
+    f = open('Projects/Spotify project/Spotify info/Albums/current_index.txt','w')
     f.write(str(x))
     f.close()
     a.close()
 
 a.close()
-c = open('Spotify project/Spotify info/Albums/current_index.txt','w')
+c = open('Projects/Spotify project/Spotify info/Albums/current_index.txt','w')
 c.write('0')
 c.close()
